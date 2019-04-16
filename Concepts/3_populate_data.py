3912066f8d4fbd01e82af3041a917690d8714e21
Joyce Liao <joyliao07@gmail.com>
2019-04-15 17:40:57 -0700

    basic django setup

--- Concepts/3_populate_data.py
+++ Concepts/3_populate_data.py
@@ -11,10 +11,12 @@ from faker import Faker
 fakegen = Faker()
 topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
 
+
 def add_topic():
     t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
     t.save()
     return t
+
 
 def populate(N=5):
     for entry in range(N):
@@ -32,4 +34,3 @@ if __name__ == '__main__':
     print('Populating script...')
     populate(20)
     print('Populating complete!')
-
