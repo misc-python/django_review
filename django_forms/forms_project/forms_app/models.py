3bf58b6574e7ed203f365aae34aa001593aeb94c
Joyce Liao <joyliao07@gmail.com>
2019-04-21 18:15:32 -0700

    install pillow and UserProfileInfo model

--- django_forms/forms_project/forms_app/models.py
+++ django_forms/forms_project/forms_app/models.py
@@ -35,13 +35,13 @@ class NewPerson(models.Model):
         return self.email
 
 
-# class UserProfileInfo(models.Model):
+class UserProfileInfo(models.Model):
 
-    #Create a one-to-one model relationship to User
+    # Create a one-to-one model relationship to User
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    portfolio = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_pics')
 
-
-
-
-
-
+    def __str__(self):
+        return self.user.username
